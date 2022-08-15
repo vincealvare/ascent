cb9ed07226a4446ebdc2a55e429df9996ed04aad
Vince Alvare <vince@Vinces-MacBook-Pro-2.local>
2022-08-14 21:48:31 -0400

    rps gui up to react part

--- rps-gui/index.mjs
+++ rps-gui/index.mjs
@@ -1,61 +1,109 @@
-import { loadStdlib } from '@reach-sh/stdlib';
+import { loadStdlib, ask } from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib();
 
-const startingBalance = stdlib.parseCurrency(100);
-const accountAlice = await stdlib.newTestAccount(startingBalance);
-const accountBob = await stdlib.newTestAccount(startingBalance);
+const isAlice = await ask.ask(
+  `Are you Alice?`,
+  ask.yesno
+);
+const who = isAlice ? 'Alice' : 'Bob';
+
+console.log(`Starting RPS as ${who}`);
+
+let account = null;
+const createAccount = await ask.ask(
+  `Would you like to create a devnet account?`,
+  ask.yesno
+);
+if(createAccount) {
+  account = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
+} else {
+  const secret = await ask.ask(
+    `What is your account secret?`,
+    (x => x)
+  );
+  account = await stdlib.newAccountFromSecret(secret);
+}
+
+let contract = null;
+if(isAlice) {
+  contract = account.contract(backend);
+  contract.getInfo().then((info) => {
+    console.log(`The contract is deployed as = ${JSON.stringify(info)}`);
+  });
+} else {
+  const info = await ask.ask(
+    `Paste the contract information:`,
+    JSON.parse
+  );
+  contract = account.contract(backend, info);
+}
 
 const format = (x) => stdlib.formatCurrency(x, 4);
-const getBalance = async (who) => format(await stdlib.balanceOf(who));
-const beforeAlice = await getBalance(accountAlice);
-const beforeBob = await getBalance(accountBob);
+const getBalance = async () => format(await stdlib.balanceOf(account));
 
-const contractAlice = accountAlice.contract(backend);
-const contractBob = accountBob.contract(backend, contractAlice.getInfo());
+const before = await getBalance();
+console.log(`Your balance is ${before}`);
 
-const HAND = ['Rock', 'Paper', 'Scissors'];
-const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
+const interact = {...stdlib.hasRandom};
 
-//hasRandom adds random number generation functionality
-const Player = (Who) => ({
-    ...stdlib.hasRandom,
-    getHand: () => {
-      const hand = Math.floor(Math.random() * 3);
-      console.log(`${Who} played ${HAND[hand]}`);
-      return hand;
-    },
-    seeOutcome: (outcome) => {
-      console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
-    },
-    informTimeout: () => {
-      console.log(`${Who} observed a timeout`);
+interact.informTimeout = () => {
+  console.log(`There was a timeout...`);
+  process.exit(1);
+};
+
+if (isAlice) {
+  const amount = await ask.ask(
+    `How much do you wanna wager?`,
+    stdlib.parseCurrency
+  );
+  interact.wager = amount;
+  interact.deadline = { ETH: 100, ALGO: 100, CFX: 1000 }[stdlib.connector];
+} else {
+  interact.acceptWager = async (amount) => {
+    const accepted = await ask.ask(
+      `Do you accept the wager of: ${format(amount)}?`,
+      ask.yesno
+    );
+    if(!accepted) {
+      process.exit(0); //0 is right away, no more blocks!
     }
-});
+  };
+}
 
-await Promise.all([
-    contractAlice.p.Alice({
-      ...Player('Alice'),
-      wager: stdlib.parseCurrency(5),
-      deadline: 10
-    }),
-    contractBob.p.Bob({
-      ...Player('Bob'),
-      acceptWager: async (amount) => { 
-        if(Math.random() <= 0.01) { //will return true 1/100 times
-          for(let i = 0; i < 10; i++) {
-            console.log(`Bob is like, really slow...`);
-            await stdlib.wait(1); //wait 1 block time
-          }
-        } else {
-          console.log(`Bob accepts the wager of ${format(amount)}.`);
-        }
-      },
-    }),
-]);
+const HAND = ['ROCK', 'PAPER', 'SCISSORS'];
+const HANDS = {
+  'Rock': 0, 'R': 0, 'r': 0, 'rock': 0,
+  'Paper': 1, 'P': 1, 'p': 1, 'paper': 1,
+  'Scissors': 2, 'S': 2, 's': 2, 'scissors': 2
+}
 
-const afterAlice = await getBalance(accountAlice);
-const afterBob = await getBalance(accountBob);
+interact.getHand = async () => {
+  const hand = await ask.ask(
+    `What hand will you play?`,
+    (x) => {
+      const hand = HANDS[x];
+      if(hand === undefined) {
+        throw Error(`Not a valid hand ${hand}`);
+      }
+      return hand;
+    }
+  );
+  console.log(`You played ${HAND[hand]}`);
+  return hand;
+};
 
-console.log(`Alice went from ${beforeAlice} to ${afterAlice}.`);
-console.log(`Bob went from ${beforeBob} to ${afterBob}.`);
+const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
+interact.seeOutcome = async (outcome) => {
+  console.log(`${OUTCOME[outcome]} the match!`);
+};
+
+
+const participant = isAlice ? contract.p.Alice : contract.p.Bob;
+await participant(interact);
+
+const after = await getBalance();
+console.log(`Your balance is now ${after}`);
+
+ask.done();
+
